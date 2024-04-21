b17eaaee2f2cb03de2083bbdcf61c860bee24991
Nikhil0504 <nikhilgaruda@live.com>
2024-04-21 15:24:09 -0700

    v 0.2.0!!!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Pipelines',
-    version='0.1.0',
+    version='0.2.0',
     author='Nikhil Garuda',
     description='Astronomical data processing pipelines',
     packages=find_packages(),
