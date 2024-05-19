6c73ddd69fc4949e19cf8bbc325df2fb75cdeb56
Nikhil Garuda <nikhilgaruda@live.com>
2024-05-18 19:23:21 -0700

    add aperture support to ds9 reg files

--- pipelines/common/parse_ds9_regions.py
+++ pipelines/common/parse_ds9_regions.py
@@ -1,5 +1,8 @@
 import re
 
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from photutils.aperture import SkyRectangularAperture
 
 def parse_ds9_reg_file(filename):
     # Regular expression to match region lines (simplified for circle, box, and ellipse)
@@ -28,3 +31,15 @@ def parse_ds9_reg_file(filename):
                 regions.append(region)
     
     return regions
+
+def get_apertures(reg_file, frame='fk5'):
+    regions = parse_ds9_reg_file(reg_file)
+
+    sky_coords = [SkyCoord(ra=float(region['ra'])*u.degree, dec=float(region['dec'])*u.degree, frame=frame) for region in regions]
+    apertures = [SkyRectangularAperture(positions=sky_coord, 
+                                        w=float(region['sizes'][0].split('\"')[0])*u.arcsec, 
+                                        h=float(region['sizes'][1].split('\"')[0])*u.arcsec, 
+                                        theta=(int(region['sizes'][2]) - 90) * u.deg )for sky_coord, region in zip(sky_coords, regions)]
+
+    return apertures
+
